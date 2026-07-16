3c56b2ebfab296e9372bb5c7a7552c8d190d6641
misha213123 <misha0228050@gmail.com>
2026-07-16 23:40:27 +0200

    Add SHIFTLY calculation utilities

---
+++ src/utils/calculations.js
@@ -0,0 +1,27 @@
+import { departments } from '../data/departments';
+
+export const money = (value) => `${Number(value || 0).toFixed(2)} zł`;
+export const pad = (value) => String(value).padStart(2, '0');
+export const dateKey = (date = new Date()) => `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}`;
+export const monthKey = (date = new Date()) => dateKey(date).slice(0, 7);
+
+export function calculateShift(quantities = {}, rates = {}) {
+  return departments.reduce(
+    (total, department) => total + Number(quantities[department.id] || 0) * Number(rates[department.id] || 0),
+    0,
+  );
+}
+
+export function totalCartons(quantities = {}) {
+  return Object.values(quantities).reduce((sum, value) => sum + Number(value || 0), 0);
+}
+
+export function getWeekRange(date) {
+  const start = new Date(date);
+  start.setHours(0, 0, 0, 0);
+  start.setDate(start.getDate() - ((start.getDay() + 6) % 7));
+  const end = new Date(start);
+  end.setDate(end.getDate() + 6);
+  end.setHours(23, 59, 59, 999);
+  return { start, end };
+}
